437915fa50a9cedb7146a5a5a21f25ebb414d6f5
PhuongPhamDuc2108 <pducphuonghd32108@gmial.com>
2024-12-13 22:50:33 +0700

    update css

--- finance/templatetags/custom_filters.py
+++ finance/templatetags/custom_filters.py
@@ -9,3 +9,12 @@ def subtract(value, arg):
         return float(value) - float(arg)
     except (ValueError, TypeError):
         return 0  # Nếu không thể thực hiện phép trừ, trả về 0
+
+
+@register.filter(name='add_class')
+def add_class(value, arg):
+    """
+    Thêm class vào phần tử form.
+    Ví dụ: {{ form.email|add_class:"form-control" }}
+    """
+    return value.as_widget(attrs={'class': arg})
